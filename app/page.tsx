f59ba955af2e525069e37c69e9f49f7ffc25102a
SagarDNG <sagardng123@gmail.com>
2025-02-07 21:08:42 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const response = await fetch("/api/generate-quotes", {
+      const response = await fetch("./api/generate-quotes", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ theme }),
